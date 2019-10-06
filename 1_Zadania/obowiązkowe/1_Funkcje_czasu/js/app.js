04cd58552875dd63b3ec2ca39e01b4051fcc975f
aureliuszek <aureliusz.wala@gmail.com>
2019-10-06 10:19:25 +0200

    Update app.js
    zadanie done

---
+++ 1_Zadania/obowiązkowe/1_Funkcje_czasu/js/app.js
@@ -0,0 +1,12 @@
+function countHello (number) {
+    var count = 0;
+    var intervalID = setInterval(() => {
+        count ++;
+    console.log('Hello', count);
+    if (count === number){
+        clearInterval(intervalID);
+    }
+},500);
+}
+
+countHello(5)
