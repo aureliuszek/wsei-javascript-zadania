1d62196b0ae4b14cb13f71b2935485326c5f040c
aureliuszek <aureliusz.wala@gmail.com>
2019-10-06 10:49:48 +0200

    Update app.js
    zadanie done

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -15,3 +15,12 @@ numberArray.forEach(element => {
    
 }
 distFromAvarage([1,2,3,4,5,6,7])
+
+//zadanie 0 
+
+let fruits = ['apple','orange','banana','peach','pear'];
+console.log(fruits[0]);
+console.log(fruits[fruits.length - 1]);
+for(let count = 0; count < fruits.length; count++){
+    console.log(fruits[count]);
+}
