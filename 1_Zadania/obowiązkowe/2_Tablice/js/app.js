e8cd88fcc731c75f2b770a25aee6b3ddd5967a67
aureliuszek <aureliusz.wala@gmail.com>
2019-10-20 10:24:15 +0200

    Update app.js
    did task 5

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -1,3 +1,4 @@
+//zadanie 0 
 function distFromAvarage(numberArray){
     var avarage = 0;
     numberArray.forEach(element => {
@@ -16,7 +17,7 @@ numberArray.forEach(element => {
 }
 distFromAvarage([1,2,3,4,5,6,7])
 
-//zadanie 0 
+//zadanie 1 
 
 let fruits = ['apple','orange','banana','peach','pear'];
 console.log(fruits[0]);
@@ -24,3 +25,33 @@ console.log(fruits[fruits.length - 1]);
 for(let count = 0; count < fruits.length; count++){
     console.log(fruits[count]);
 }
+
+//zadanie 4 
+function multiply(array){
+    var multiplication=1;
+    for(let i=0; i<array.length; i++){
+        multiplication=multiplication*array[i];
+    }
+    console.log(multiplication);
+}
+
+multiply([1,2,3,4,5,6,7]);
+//zadanie 5
+function getEvenAvarage(array){
+    let avg = 0;
+    let evenNumber = 0;
+    array.forEach(element => {
+        if(element % 2 == 0){
+            avg += element
+            evenNumber ++
+        }
+    });
+    if(avg == 0){
+        console.log(null)
+    } else {
+        console.log(avg/evenNumber)
+    }
+}
+getEvenAvarage([1,2,3,4,5,6,7])
+getEvenAvarage([1,1,1,1])
+getEvenAvarage([2,8,3,7,4])
