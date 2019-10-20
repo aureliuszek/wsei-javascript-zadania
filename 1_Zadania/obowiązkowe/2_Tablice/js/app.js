9adb688be2dad6bd4e3158a9e11d1585e268be1f
aureliuszek <aureliusz.wala@gmail.com>
2019-10-20 10:34:07 +0200

    Update app.js
    added task 6

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -25,6 +25,13 @@ console.log(fruits[fruits.length - 1]);
 for(let count = 0; count < fruits.length; count++){
     console.log(fruits[count]);
 }
+//zadanie 3 
+function printTable(array){
+    for(let i = 0; i <= array.length; i++){
+        console.log(array[i]);
+    }
+}
+printTable([1,3,4,5,6])
 
 //zadanie 4 
 function multiply(array){
@@ -34,8 +41,8 @@ function multiply(array){
     }
     console.log(multiplication);
 }
-
 multiply([1,2,3,4,5,6,7]);
+
 //zadanie 5
 function getEvenAvarage(array){
     let avg = 0;
@@ -55,3 +62,23 @@ function getEvenAvarage(array){
 getEvenAvarage([1,2,3,4,5,6,7])
 getEvenAvarage([1,1,1,1])
 getEvenAvarage([2,8,3,7,4])
+
+//zadanie 6
+
+function sortArray(array){
+    function compareNumbers(arrayFirstValue, arraySecondValue){
+        if(arrayFirstValue < arraySecondValue){
+            return -1;
+        }
+        if(arrayFirstValue > arraySecondValue){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+    console.log(array.sort(compareNumbers));
+}
+
+sortArray([145,11,3,64,4,6,10]);
+
