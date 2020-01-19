cfb04b9df21dd3b32eea67b5217f3bd80a607cdb
aureliuszek <aureliusz.wala@gmail.com>
2020-01-19 21:55:00 +0100

    did obiekty tasks

---
+++ 1_Zadania/obowiązkowe/4_Obiekty/js/app.js
@@ -0,0 +1,63 @@
+//zadanie 0a
+var city = {
+    capital  : String,
+    population : Number,
+    president : String,
+    primeMinisters : String = []
+  };
+  console.log(city)
+
+//zadanie 0b
+  var timeMachine = {
+    shape: 'rectangle',
+    model: 'new',
+    run: function(date, place){
+        console.log(date + " " + place)
+    }
+}
+for(let atributes in timeMachine){
+    console.log(timeMachine[atributes])
+}
+timeMachine.run("1986-04-26", "Chernobyl")
+
+//zadanie 1
+const book = {
+    title : "The Lord of the Rings",
+    author : "J. R. R. Tolkien",
+    numberOfPages : 1345
+}
+for(let atributes in book){
+    console.log(book[atributes])
+}
+
+//zadanie 2
+const person = {
+    name : "Aureliusz",
+    age : 22,
+    sayHello: function(){
+        console.log('Hello')
+    }
+}
+for(let atributes in person){
+    console.log(person[atributes])
+}
+person.sayHello()
+
+//zadanie 3 
+const recipe = {
+    title : "Jajecznica z boczkiem i pomidorem",
+    servings : 2
+}
+recipe.ingredients = ['6 jajek', '200g boczku', '1 pomidor']
+for(let atributes in recipe){
+    console.log(recipe[atributes])
+}
+
+//zadanie 6 
+var spoon = {
+    isExist: true
+}
+
+var fork = spoon;
+fork.isExist  = false;
+//łyżka nie isnieje - wlasciwosc fork przechodzi na spoon
