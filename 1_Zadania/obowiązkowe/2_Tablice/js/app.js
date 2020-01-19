9bac06b04b8d15fdc3ce9ba18a3ef675745a5b5c
aureliuszek <aureliusz.wala@gmail.com>
2020-01-19 21:16:31 +0100

    did some tasks

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -16,18 +16,20 @@ numberArray.forEach(element => {
    
 }
 distFromAvarage([1,2,3,4,5,6,7])
+distFromAvarage([1,1,1,1])
+distFromAvarage([2,8,3,7])
 
 //zadanie 1 
 
-let fruits = ['apple','orange','banana','peach','pear'];
+var fruits = ['apple','orange','banana','peach','pear'];
 console.log(fruits[0]);
 console.log(fruits[fruits.length - 1]);
-for(let count = 0; count < fruits.length; count++){
+for(var count = 0; count < fruits.length; count++){
     console.log(fruits[count]);
 }
 //zadanie 3 
 function printTable(array){
-    for(let i = 0; i <= array.length; i++){
+    for(var i = 0; i <= array.length; i++){
         console.log(array[i]);
     }
 }
@@ -42,6 +44,8 @@ function multiply(array){
     console.log(multiplication);
 }
 multiply([1,2,3,4,5,6,7]);
+multiply([1,1,1,1]);
+multiply([2,8,3,7]);
 
 //zadanie 5
 function getEvenAvarage(array){
@@ -84,15 +88,16 @@ sortArray([145,11,3,64,4,6,10]);
 
 //zadanie 7
 
-function addArrays(firstArray, secondArray){
-    let sumArrays = [];
-    for(let i = 0; i < firstArray.length; i++){
-        for(let j = 0; j < secondArray.length; j++)
-        {
-            Array.push.apply(firstArray[1] , secondArray[j]);
-        }
+function addArrays(first, second){
+    let counter = first.length > second.length ? first.length : second.length
+    let result = []
+    for(let i = 0; i < counter; i++){
+        let firstNum = first[i] != null ? first[i] : 0
+        let secondNum = second[i] != null ? second[i] : 0
+        result[i] = firstNum + secondNum
     }
-    console.log(firstArray);   
+    return result
 }
-addArrays([4,0,1,3,4], [1,9,6,7,8,17]) // do skonczenia
-
+addArrays([4,0,1,3,4], [1,9,6,7,8,17]);
+addArrays([8,3,22], [1,3,2]); 
+addArrays([2,3,1,5,3,5], [3,1,76,1]);
